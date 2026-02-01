0fff88ae91c49ede88855b12bdc8d3f8df859dfc
FilipPopescu <filippopescu18@gmail.com>
2026-02-01 21:03:35 +0200

    Added separate NewPaymentFragment and RecurringPaymentFragment

---
+++ EBanking/app/src/main/java/com/example/e_banking/NewPaymentFragment.kt
@@ -0,0 +1,14 @@
+package com.example.e_banking
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class NewPaymentFragment : Fragment(R.layout.payment_new) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize views, listeners, logic here
+    }
+}
