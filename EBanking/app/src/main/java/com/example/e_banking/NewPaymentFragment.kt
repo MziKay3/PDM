8821cc8fc5b5290e17b2329852b6a546d425565c
FilipPopescu <filippopescu18@gmail.com>
2026-02-01 21:18:32 +0200

    Implemented NewPaymentFragment (except the actual payment part)

--- EBanking/app/src/main/java/com/example/e_banking/NewPaymentFragment.kt
+++ EBanking/app/src/main/java/com/example/e_banking/NewPaymentFragment.kt
@@ -2,13 +2,71 @@ package com.example.e_banking
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.e_banking.api.APICaller
+import com.example.e_banking.api.callbacks.DefaultCallback
+import com.example.e_banking.api.dtos.AccountDetails
+import kotlin.properties.Delegates
 
 class NewPaymentFragment : Fragment(R.layout.payment_new) {
+
+    private var balance by Delegates.notNull<Float>()
+    private var selectedRecurrence: Recurrence = Recurrence.NONE
+
+    private lateinit var fromTextView: TextView
+    private lateinit var beneficiaryNameEditText: EditText
+    private lateinit var beneficiaryAccountEditText: EditText
+    private lateinit var paymentAmountEditText: EditText
+    private lateinit var paymentDetailsEditText: EditText
+    private lateinit var recurrenceSpinner: Spinner
+    private lateinit var continueButton: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Initialize views, listeners, logic here
+        fromTextView = view.findViewById(R.id.fromAccountNumber)
+        beneficiaryNameEditText = view.findViewById(R.id.beneficiaryName)
+        beneficiaryAccountEditText = view.findViewById(R.id.beneficiaryAccount)
+        paymentAmountEditText = view.findViewById(R.id.paymentAmount)
+        paymentDetailsEditText = view.findViewById(R.id.paymentDetails)
+        recurrenceSpinner = view.findViewById(R.id.recurrenceSpinner)
+        continueButton = view.findViewById(R.id.btnContinue)
+
+        val accountDetailsCallback = DefaultCallback<AccountDetails>(
+            onSuccess = {
+                response ->
+                fromTextView.text = response.iban
+                balance = response.balance
+            },
+            onFailure = {
+                    response ->
+                response.errorBody()?.let {
+                    val message = it.string()
+                    Toast.makeText(requireContext(), message, Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        )
+        APICaller.getAccountDetails(accountDetailsCallback)
+
+        val recurrenceValues = Recurrence.entries.toTypedArray()
+        recurrenceSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View?,
+                    position: Int,
+                    id: Long) {
+                    selectedRecurrence = recurrenceValues[position]
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {}
+            }
     }
 }
