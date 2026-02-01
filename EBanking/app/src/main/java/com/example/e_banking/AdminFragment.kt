900f080ce34f1c7d1b7dcd8d4d7b0203b0cc4304
FilipPopescu <filippopescu18@gmail.com>
2026-02-01 20:15:59 +0200

    Implemented update userdetails logic

--- EBanking/app/src/main/java/com/example/e_banking/AdminFragment.kt
+++ EBanking/app/src/main/java/com/example/e_banking/AdminFragment.kt
@@ -1,6 +1,5 @@
 package com.example.e_banking
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -17,14 +16,14 @@ import com.example.e_banking.api.APICaller
 import com.example.e_banking.api.SecurityContext
 import com.example.e_banking.api.SecurityContextAccessor
 import com.example.e_banking.api.callbacks.DefaultCallback
+import com.example.e_banking.api.dtos.UpdateUserDetails
 import com.example.e_banking.api.dtos.UserDetails
 
 class AdminFragment : Fragment(), SecurityContextAccessor {
-    var initialPhone: String? = null
-    var initialName: String? = null
-    var initialPassword: String? = null
-
-    var onUserDetailsGetCallback: DefaultCallback<UserDetails>? = null
+    lateinit var initialPhone: String
+    lateinit var initialName: String
+    lateinit var initialPassword: String
+    lateinit var onUserDetailsGetCallback: DefaultCallback<UserDetails>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,7 +46,7 @@ class AdminFragment : Fragment(), SecurityContextAccessor {
         val inputPhone = view.findViewById<EditText>(R.id.inputPhone)
         val inputPassword = view.findViewById<EditText>(R.id.inputPassword)
 
-        onUserDetailsGetCallback = DefaultCallback<UserDetails>(
+        onUserDetailsGetCallback = DefaultCallback(
             onSuccess = {
                 response ->
 
@@ -71,11 +70,40 @@ class AdminFragment : Fragment(), SecurityContextAccessor {
                 }
             }
         )
-        APICaller.getUserDetails(onUserDetailsGetCallback!!)
+        APICaller.getUserDetails(onUserDetailsGetCallback)
 
         val btnEditName = view.findViewById<ImageButton>(R.id.btnEditName)
         val btnEditPhone = view.findViewById<ImageButton>(R.id.btnEditPhone)
         val btnEditPassword = view.findViewById<ImageButton>(R.id.btnEditPassword)
+        val btnUpdate = view.findViewById<Button>(R.id.update_user_details_button)
+        btnUpdate.setOnClickListener {
+            val name = if (inputName.isEnabled) inputName.text.toString()
+            else initialName
+            val phoneNumber = if (inputPhone.isEnabled) inputPhone.text.toString()
+            else initialPhone
+            val password = if (inputPassword.isEnabled) inputPassword.text.toString()
+            else initialPassword
+            val updateUserDetails = UpdateUserDetails(
+                name = name,
+                phoneNumber = phoneNumber,
+                password = password
+            )
+            APICaller.updateUserDetails(updateUserDetails,
+                DefaultCallback(
+                    onSuccess = {
+                        _ ->
+                        APICaller.getUserDetails(onUserDetailsGetCallback)
+                    },
+                    onFailure = {
+                        response ->
+                        response.errorBody()?.let {
+                            val message = it.string()
+                            Toast.makeText(requireContext(), message, Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                ))
+        }
 
         btnEditName.setOnClickListener {
             if (!inputName.isEnabled) {
